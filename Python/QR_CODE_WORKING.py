c37832a70faafb4a4ad05c5617c89638ecdca351
root <root@ip-172-31-60-94.ec2.internal>
2024-05-27 06:55:04 +0000

    QR_Code modified

--- Python/QR_CODE_WORKING.py
+++ Python/QR_CODE_WORKING.py
@@ -4,11 +4,11 @@ import pyqrcode
 from pyqrcode import QRCode 
   
 # String which represent the QR code 
-s = "https://www.youtube.com/channel/UCBz4yaxNxfiz1XYh-07UfWQ"
+s = "https://github.com/mniet2009/tools_installation_scripts.git"
   
 # Generate QR code 
 url = pyqrcode.create(s) 
   
 # Create and save the png file naming "myqr.png" 
 #Scalable Vector Graphics
-url.svg("myyoutube.svg", scale = 8) 
+url.svg("maruti.svg", scale = 8) 
